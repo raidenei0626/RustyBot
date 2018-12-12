c348cbebfba57b90fd796ad1cb1793e675283ff2
MattCSmith <mrmcchow@gmail.com>
2018-12-12 09:58:31 +0000

    Added stats command

---
+++ commands/stats.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ["s"],
+    permLevel: "User"
+};
+
+exports.help = {
+    name: "stats",
+    category: "Miscelaneous",
+    description: "Member Stats",
+    usage: "stats [username]"
+};
+
+
+exports.run = (client, message, args) => { // eslint-disable-line no-unused-vars
+    let name;
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    console.log(member)
+
+    if (!member) name = message.author.username + "#" + message.author.discriminator
+    else name = member.user.username + "#" + member.user.discriminator;
+
+    client.userDB.findOne({
+        name: name
+    }, (err, userr) => {
+        if (err) console.log(err);
+        if (!userr) {
+            message.reply("We have not data for this user")
+        } else {
+            fields = [["Joined ZTM", userr.joined, true], ["Joined Discord", userr.created, true], ["Messages Sent", userr.stats.mCount, true], ["Commands Issues", userr.stats.cmd, true]]
+            thumb = client.users.get(message.author.id).avatarURL
+            client.sendembed(message.channel, "User stats", thumb, "Description here", fields, "#000", boolean = true)
+        }
+    })
+
+}
