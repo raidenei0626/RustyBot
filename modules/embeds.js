ebdf03d86539a8090f045cd9f4608b90eeefec02
Matt Smith <mrmcchow@gmail.com>
2018-12-28 19:40:34 +0000

    Embed function update

--- modules/embeds.js
+++ modules/embeds.js
@@ -1,13 +1,19 @@
 module.exports = (client) => {
     const Discord = require("discord.js");
 
-    client.sendembed = (method, author, thumb, desc, fields, colour, boolean = false) => {
+    client.sendembed = (argsObj) => {
+        const {colour, title, url, author, thumb, desc, image, footer, fields} = argsObj
         let embed101 = new Discord.RichEmbed()
+            
+            .setColor(colour)
+            .setTitle(title)
+            .setURL(url)
             .setAuthor(author)
             .setThumbnail(thumb)
-            .setColor(colour)
             .setDescription(desc)
-
+            .setImage(image)
+            .setFooter(footer)
+            
             if(fields){
                 fields.forEach((f, i) => {
                     embed101.addField(f[0], f[1], f[2])
