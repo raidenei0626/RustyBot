15645dc003a2dd1f27e60cc6b88e7a79bdf53ba6
Sherin Binu <sherinbinu@hotmail.com>
2019-01-08 23:04:36 +0530

    report issue to issueTracker

--- commands/report.js
+++ commands/report.js
@@ -11,3 +11,10 @@ exports.help = {
   description: 'Report an issue to Admin team',
   usage: 'report [MESSAGE]'
 }
+
+exports.run = (client, message, args) => {
+  let author = message.author
+  let issue = args.join(' ');
+  if(client.channels.has(client.settings.issueTracker)) client.channels.get(client.settings.issueTracker).send(author + ' reported an issue: ' + issue)
+  
+}
