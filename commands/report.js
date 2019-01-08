c54615b2763f3c0001774413d4cb737ea1214f29
Sherin Binu <sherinbinu@hotmail.com>
2019-01-08 20:54:31 +0530

    set conf and hep for report command

---
+++ commands/report.js
@@ -0,0 +1,13 @@
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 'User'
+}
+
+exports.help = {
+  name: 'report',
+  category: 'Miscelaneous',
+  description: 'Report an issue to Admin team',
+  usage: 'report [MESSAGE]'
+}
